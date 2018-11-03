de4046e7d992b3e3f8928d085a18c9722c0f707d
zouyajie <jyz4872@163.com>
2018-11-03 16:01:47 +0800

    (*)优化dbscan代码

--- dbscan.py
+++ dbscan.py
@@ -2,16 +2,24 @@
 # coding:utf-8
 import numpy as np
 import matplotlib.pyplot as plt
-
 ## 模型数据
 import pandas as pd
+from sklearn.cluster import DBSCAN
 import random
 import pickle
-# from matplotlib import plt
-all_data = pd.read_csv("D:\\receive_power_outage.csv")
+from sklearn.preprocessing import StandardScaler
+
+
+all_data = pd.read_csv("D:\\receive_power_outage2.csv")
 # pointtt = list(all_data.iloc[:, 1:3])
 singla = all_data.iloc[:, 4:]
-y_si = DBSCAN(eps=10, min_samples=6, algorithm='ball_tree').fit_predict(singla)
+
+std = StandardScaler()
+singla = std.fit_transform(singla)
+# X_test = std.transform(X_test)
+
+# 归一化前参数组合为：eps=10, min_samples=6
+y_si = DBSCAN(eps=1.2, min_samples=3).fit_predict(singla)  # , algorithm='ball_tree'
 print(y_si)
 
 # 读取代码计算出的点坐标
@@ -20,10 +28,10 @@ hot_end = pickle.load(point_file)
 round_end = pickle.load(point_file)
 roundfocu = pickle.load(point_file)
 third_layer_focu = pickle.load(point_file)
-pointtt = pickle.load(point_file)
+# pointtt = pickle.load(point_file)
 
 
-def display(hot_endpoint, round_endpoint, roundfocus, third_layer_focus, points):
+def display(hot_endpoint, round_endpoint, roundfocus, third_layer_focus):
     # 展示中心正六边形到坐标系上
     hot_x = []
     hot_y = []
@@ -54,22 +62,25 @@ def display(hot_endpoint, round_endpoint, roundfocus, third_layer_focus, points)
     plt.scatter(focus_x, focus_y, marker='*', color='r', s=10)
 
     # 展示UE_points到坐标系上
-    point_x = []
-    point_y = []
-    for p in range(len(points)):
-        point_x.append(points[p][0])
-        point_y.append(points[p][1])
-    plt.scatter(point_x, point_y, marker='+', color='b', s=10)
+    # point_x = []
+    # point_y = []
+    # for p in range(len(points)):
+    #     point_x.append(points[p][0])
+    #     point_y.append(points[p][1])
+    # plt.scatter(point_x, point_y, marker='+', color='b', s=10)
     plt.xlim(-6, 6)
     plt.ylim(-6, 6)
     # plt.show()
 
 
-display(hot_end, round_end, roundfocu, third_layer_focu, pointtt)
+display(hot_end, round_end, roundfocu, third_layer_focu)
 coord = all_data.iloc[:, :4].values
 # idd = [i for i, x in enumerate(coord) if x[-1] == 1]
 label_si = list(set(y_si))
-a = ["b", "r", "y", "c", "coral"]
+a = ["b", "r", "y", "g", "k", "m", "c", "coral"]
+# ['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+#     'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+#     'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
 # b = random.sample(a, 1)[0]
 for lsi in label_si:
     idd = [i for i, x in enumerate(y_si) if x == lsi]
@@ -82,7 +93,6 @@ for lsi in label_si:
     plt.legend(loc='upper left')
 plt.show()
 i = 0
-
 
 
 
